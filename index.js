1d49a430e8e89e37eba89d5658c8a661b0388653
SanyamMadaan <124160436+SanyamMadaan@users.noreply.github.com>
2023-02-01 18:38:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const endDate = "21 February 2023 12:00 AM" ;
+document.getElementById('end').innerText=endDate;
+const inputs=document.querySelectorAll("input")
+function clock(){
+const now= new Date() ;
+const end= new Date(endDate) ;
+const diff = (end -now)/1000 ;
+if(diff<0) return;
+inputs[0].value= Math.floor(diff/3600/24);
+inputs[1].value= Math.floor(diff/3600)%24;
+inputs[2].value= Math.floor(diff/60)%60;
+inputs[3].value= Math.floor(diff)%60;
+}
+clock();
+
+setInterval ( ()=> {clock()},
+1000
+)
